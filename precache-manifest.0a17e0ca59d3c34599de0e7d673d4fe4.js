775ae07d35b3b2416b8cc18a6fe553542cca6e37
fabio almeida <fabioalmeida@ge.com>
2020-05-15 15:23:41 -0400

    Updates

---
+++ precache-manifest.0a17e0ca59d3c34599de0e7d673d4fe4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa75218ebef9019538e21bd155bbf90",
+    "url": "/portifolio/index.html"
+  },
+  {
+    "revision": "8c2ff0326f965cf7d204",
+    "url": "/portifolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3219697ab537ec427179",
+    "url": "/portifolio/static/css/main.a8c74213.chunk.css"
+  },
+  {
+    "revision": "8c2ff0326f965cf7d204",
+    "url": "/portifolio/static/js/2.7d16c133.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portifolio/static/js/2.7d16c133.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3219697ab537ec427179",
+    "url": "/portifolio/static/js/main.a1241241.chunk.js"
+  },
+  {
+    "revision": "a8445369021075fa2242",
+    "url": "/portifolio/static/js/runtime-main.fa749dad.js"
+  },
+  {
+    "revision": "e9171d0c6ccc850dfec2a318122c17a3",
+    "url": "/portifolio/static/media/IMG_0827.e9171d0c.JPG"
+  }
+]);
